35c2e7a9ef2213517f96d6d1764f71900e5e5362
ekb2024 <igor_nu@bk.ru>
2024-07-24 02:20:26 +0500

    Update homedop.py

--- homedop.py
+++ homedop.py
@@ -1,7 +1,7 @@
 students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
 grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
-average_ball ={}
+average_ball = {}
 
 for i in range(len(students)):
-    average_ball[sorted(list(students))[i]] = sum(grades[i])/len(grades[i])
+    average_ball[sorted(list(students))[i]] = sum(grades[i]) / len(grades[i])
 print(average_ball)
