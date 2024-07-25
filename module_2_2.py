04625b85ef33bbe0ed1552538e1a136f9267e4e4
ekb2024 <igor_nu@bk.ru>
2024-07-26 01:25:35 +0500

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,10 @@
+first =  input('введите первое число :')
+second = input('введите второе число :')
+third =  input('введите третье число :')
+
+if first == second and second == third:
+    print('3')
+elif first == second or second == third or first == third:
+    print('2')
+else:
+    print('0')
