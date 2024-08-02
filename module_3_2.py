20da75c9c44b3b55b466db3515ff9950357b9233
ekb2024 <igor_nu@bk.ru>
2024-08-02 17:48:12 +0500

    Update module_3_2.py

--- module_3_2.py
+++ module_3_2.py
@@ -6,12 +6,12 @@ def send_email(message, recipient, sender="university.help@gmail.com"):
              if i_2 == "@":
                 for n_1 in domens:
                   if recipient[len(recipient) - len(n_1):len(recipient)] == n_1:
-                       for n_2 in domens:
-                          if sender[len(sender) - len(n_2):len(sender)] == n_2:
-                             if sender == "university.help@gmail.com":
-                                return print("Письмо успешно отправлени с адреса", sender,"на адрес",recipient)
-                             elif recipient == sender:
-                                return print("Невозможно отправить письмо самомму себе")
-                             else:
-                                return print("НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с", sender,"на адрес", recipient)
+                     for n_2 in domens:
+                        if sender[len(sender) - len(n_2):len(sender)] == n_2:
+                            if sender == "university.help@gmail.com":
+                             return print("Письмо успешно отправлени с адреса", sender,"на адрес",recipient)
+                            elif recipient == sender:
+                             return print("Невозможно отправить письмо самомму себе")
+                            else:
+                             return print("НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с", sender,"на адрес", recipient)
     return print("Невозможно отправить письмо c адреса", sender, "на адрес", recipient)
