7794155de53c34794755daa454a1ab802f8e1a8c
ekb2024 <igor_nu@bk.ru>
2024-09-01 02:59:48 +0500

    Домашнее задание по теме "Зачем нужно наследование"
    применить базовые знания о наследовании классов для решения задачи

---
+++ module_6/module_6_1.py
@@ -0,0 +1,46 @@
+class Animal:
+    def __init__(self, name, alive=True, fed=False):
+        self.alive = alive
+        self.fed = fed
+        self.name = name
+class Plant:
+    def __init__(self, name, edible=False):
+        self.edible = edible
+        self.name = name
+class Mammal(Animal):
+    def eat(self, food):
+        if food.edible == False:
+            self.alive = False
+            print(f'{self.name} не стал есть {food.name}')
+        else:
+            self.fed = True
+            print(f'{self.name} съел {food.name}')
+class Predator(Animal):
+    def eat(self, food):
+          if  food.edible == False:
+              self.alive = False
+              print( f'{self.name} не стал есть {food.name}')
+          else:
+              self.fed = True
+              print(f'{self.name} съел {food.name}')
+class Flower(Plant):
+      pass
+class Fruit(Plant):
+    def __init__(self, name, edible=True):
+        self.edible = True
+        self.name = name
+
+a1 = Predator('Волк с Уолл-Стрит')
+a2 = Mammal('Хатико')
+p1 = Flower('Цветик семицветик')
+p2 = Fruit('Завондной апельсин')
+print(a1.name)
+print(p1.name)
+print(a1.alive)
+print(a2.fed)
+a1.eat(p1)
+a2.eat(p2)
+print(a1.alive)
+print(a2.fed)
+
+
