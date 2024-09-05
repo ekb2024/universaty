077970d30c7511c1d1f91f4ef05ea39830b6128b
ekb2024 <igor_nu@bk.ru>
2024-09-05 13:20:02 +0500

    Update (module_6_2.py

--- module_6/(module_6_2.py
+++ module_6/(module_6_2.py
@@ -1,27 +1,27 @@
 class Vehicle:
+    _COLOR_VARIANTS = ['red', 'blue', 'green', 'black', 'whlle']
     def __init__(self,owner,_model,_color,_engine):
        self.owner = owner
        self._model = _model
        self._color = _color
        self._engine = _engine
-       self._COLOR_VARIANTS = ['Red','Blue','Green','Black','Whlle']
+
     def get_model(self):
         return f'Модель :{self._model}'
     def get_horsepower(self):
         return f'Мощность двигателя {self._engine}'
     def get_color(self):
         return f'Цвет:{self._color}'
-    def set_color(self,new_color):
-        for i in self._COLOR_VARIANTS:
-            if i.upper() == new_color.upper():
-               self._color = new_color.upper()
-               self._color = "\033[32m{}".format(self._color)
-               return
-        print("\033[31m{}".format(f'Нельзя сменить цвет на {new_color}'),"\033[0m")
+    def set_color(self, new_color):
+        if new_color.lower() in self._COLOR_VARIANTS:
+            self._color = "\033[32m{}".format(new_color)
 
+        else:
+         print("\033[31m{}".format(f'Нельзя сменить цвет на {new_color}'),"\033[0m")
 
     def print_info(self):
-        print(f'{self.get_model()}\n{self.get_horsepower()}\n{self.get_color()}\nВладелец:{self.owner}')
+        print(f'{self.get_model()}\n{self.get_horsepower()}\n{self.get_color()}\nВладелец:{self.owner}',"\033[0m")
+
 class Sedan(Vehicle):
     _PASSENGERS_LIMIT = 5
 
