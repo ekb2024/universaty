677dd94b2614694bdf4173902be78d096362a51b
ekb2024 <igor_nu@bk.ru>
2024-09-06 02:44:34 +0500

    Домашнее задание по теме "Множественное наследование"
    Цель: закрепить знания множественного наследования в Python.

---
+++ module_6/module_6_3.py
@@ -0,0 +1,38 @@
+class Horse:
+    def __init__(self,x_distance=0,y_distance=0,sound='Frrr'):
+        self.x_distance = x_distance
+        self.sound = sound
+        super().__init__(y_distance)
+    def run(self,dx):
+        self.x_distance += dx
+class Eadle:
+    def __init__(self,y_distance=0,sound='I train,eat,sleep,and repeat'):
+        self.y_distance = y_distance
+        self.sound = sound
+
+    def fly(self,dy):
+        self.y_distance += dy
+class Pegasus(Horse,Eadle):
+    def move(self,dx,dy):
+        self.run(dx)
+        self.fly(dy)
+
+    def get_pos(self):
+        return (self.x_distance,self.y_distance)
+
+    def voice(self):
+        print(self.sound)
+
+
+#print(Pegasus.mro())
+p1 = Pegasus()
+print(p1.get_pos())
+p1.move(10,15)
+print(p1.get_pos())
+p1.move(-5,20)
+print(p1.get_pos())
+p1.voice()
+
+
+
+
