463c5898ef9617308186783930a297bc8914b8ca
ekb2024 <igor_nu@bk.ru>
2024-10-08 03:35:50 +0500

    Домашнее задание по теме "Блокировки и обработка ошибок"

---
+++ module_10/module_10_3.py
@@ -0,0 +1,45 @@
+from threading import Thread,Lock
+from random import randint
+from time import sleep
+
+class Bank:
+    def __init__(self):
+        self.balance = 0
+        self.lock = Lock()
+
+        super().__init__()
+    def deposit(self):
+           random_ = 0
+           for i in range(100):
+                random_ = randint(50, 500)
+                self.balance += random_
+                print(f'Пополнение: {random_}. Баланс: {self.balance}')
+                sleep(0.001)
+                if self.balance >= 500 and self.lock.locked() == True:
+                  self.lock.release()
+
+
+    def take(self):
+        for i in range(100):
+            random_ = randint(50, 500)
+            print(f'Запрос на {random_}')
+            if random_ <= self.balance:
+                self.balance -= random_
+                sleep(0.001)
+                print(f'Снятие:{random_}. Баланс: {self.balance}')
+            else:
+                print('Запрос отклонён, недостаточно средств')
+                self.lock.acquire()
+
+
+
+bk = Bank()
+th1 = Thread(target=Bank.deposit, args=(bk,))
+th2 = Thread(target=Bank.take, args=(bk,))
+
+th1.start()
+th2.start()
+th1.join()
+th2.join()
+
+print(f'Итоговый баланс: {bk.balance}')
