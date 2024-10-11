1b27af6317605f6d6a0f5bc2f11f0f305f72cfd7
ekb2024 <igor_nu@bk.ru>
2024-10-11 15:57:01 +0500

    Update module_10_1.py

--- module_10/module_10_1.py
+++ module_10/module_10_1.py
@@ -1,27 +1,36 @@
-from threading import Thread
 from time import sleep
-class Knight(Thread):
-    def __init__(self, name, power):
-        self.name_ = name
-        self.power_ = power
-        super().__init__()
-    def run(self):
-        print(self.name_,'на нас напали!')
-        n_day = 0
-        n_life = 100
-        while n_life >= self.power_:
-           n_life -= self.power_
-           n_day += 1
-           sleep(1)
-           print(f'{self.name_} сражается {n_day} дней ..., осталось {n_life} воинов')
-
-        print(f'{self.name_} одержал победу спустя {n_day} дней(дня)!')
+from datetime import datetime
+from threading import Thread
+def  write_words(word_count, file_name):
+    with open(file_name,'w',encoding='utf-8') as file:
+       for i in range(1,word_count+1):
+           file.write(f'Какое-то слово № {i}\n')
+           sleep(0.1)
+    print(f'Завершилась запись в файл{file_name}')
 
 
-first_knight = Knight('Sir Lancelot', 10)
-second_knight = Knight("Sir Galahad", 20)
+time_start = datetime.now()
+write_words(10, 'example1.txt')
+write_words(30, 'example2.txt')
+write_words(200, 'example3.txt')
+write_words(100, 'example4.txt')
+print(f'Работа потоков {datetime.now() - time_start}')
 
-first_knight.start()
-second_knight.start()
-first_knight.join()
-second_knight.join()
+time_start = datetime.now()
+thread_1 = Thread(target=write_words,args=[10, 'example5.txt'])
+thread_2 = Thread(target=write_words,args=[30, 'example6.txt'])
+thread_3 = Thread(target=write_words,args=[200, 'example7.txt'])
+thread_4 = Thread(target=write_words,args=[100, 'example8.txt'])
+
+thread_1.start()
+thread_2.start()
+thread_3.start()
+thread_4.start()
+
+thread_1.join()
+thread_2.join()
+thread_3.join()
+thread_4.join()
+
+print(f'Работа потоков {datetime.now() - time_start}')
+
