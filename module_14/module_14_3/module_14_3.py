f64ea20b2fdd82c35ce81f956285c6620455515d
ekb2024 <igor_nu@bk.ru>
2024-11-30 02:11:58 +0500

    Update module_14_3.py

--- module_14/module_14_3/module_14_3.py
+++ module_14/module_14_3/module_14_3.py
@@ -5,7 +5,7 @@ from aiogram.types import ReplyKeyboardMarkup,KeyboardButton
 from aiogram.types import InlineKeyboardMarkup ,InlineKeyboardButton
 import asyncio
 
-api = ''
+api = '7895099985:AAHAaLI6o5Tx21o_jOwSepvQvDlbYJaNJCQ'
 bot = Bot(token=api)
 dp = Dispatcher(bot,storage=MemoryStorage())
 
@@ -21,23 +21,12 @@ button3 = KeyboardButton(text='Купить',callback_data='formulas')
 kb.row(button1,button2)
 kb.add(button3)
 
-menu = InlineKeyboardMarkup(
-    inline_keyboard=[
-        [InlineKeyboardButton(text='Product1', callback_data="product_buying")],
-        [InlineKeyboardButton(text='Product2', callback_data="product_buying")],
-        [InlineKeyboardButton(text='Product3', callback_data="product_buying")],
-        [InlineKeyboardButton(text='Product4', callback_data="product_buying")]
-    ],
-resize_keyboard=True
-)
-
-
-kb1 = InlineKeyboardMarkup(resize_keyboard=True)
+menu = InlineKeyboardMarkup(resize_keyboard=True)
 b1 = InlineKeyboardButton(text='Product1', callback_data="product_buying")
 b2 = InlineKeyboardButton(text='Product2', callback_data="product_buying")
 b3 = InlineKeyboardButton(text='Product3', callback_data="product_buying")
 b4 = InlineKeyboardButton(text='Product4', callback_data="product_buying")
-kb1.row(b1,b2,b3,b4)
+menu.row(b1,b2,b3,b4)
 
 @dp.message_handler(commands=['start'])
 async  def start(message):
@@ -50,15 +39,12 @@ async def get_buying_list(message):
       await message.answer(f'Название: Product {i} Описание: {list_vit[i-1]}  Цена: {i*100}')
       with open(f'files/img{i}.jpg', 'rb') as img:
          await message.answer_photo(img)
-    await message.answer('Выбирите продукт для покупки:', reply_markup=kb1)
+    await message.answer('Выбирите продукт для покупки:', reply_markup=menu)
 
 @dp.callback_query_handler(text="product_buying")
 async def send_confirm_message(call):
     await call.message.answer("Вы успешно приобрели продукт!")
     await call.answer()
-
-
-
 
 @dp.message_handler(text='Рассчитать')
 async  def set_age(message):
