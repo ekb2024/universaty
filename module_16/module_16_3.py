64a57cb2abc860eaddac958131c29fd56554541f
ekb2024 <igor_nu@bk.ru>
2024-12-16 16:18:49 +0500

    Add files via upload

---
+++ module_16/module_16_3.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI,Path
+from typing import Annotated
+users = {'1': 'Имя: Example, возраст: 18'}
+app = FastAPI()
+@app.get("/users")
+async  def get_message( ) ->dict:
+     return users
+
+@app.post('/user/{username}/{age}')
+async  def append_user(username :Annotated[str,Path(min_length=3,max_length=20)]
+                       ,age:int=Path(ge=18, le=120))->str:
+     index = str(int(max(users, key=int)) + 1)
+     users[index] = f'Имя: {username} ,возраст: {age}'
+     return f"User {index}  is registered "
+
+@app.put('/user/{user_id}/{username}/{age}')
+async def update_user(user_id: int=Path(ge=1, le=int(max(users, key=int))),
+                        username: str=Path(min_length=3,max_length=20),
+                        age: int=Path(ge=18, le=120)) ->str:
+     users[str(user_id)] =  f'Имя: {username} ,возраст: {age}'
+     return f'The user {str(user_id)} is updated'
+
+@app.delete('/user/{user_id}')
+async def delete_user(user_id: int=Path(ge=1, le=int(max(users, key=int)))) ->str:
+     users.pop(str(user_id))
+     return f"User {str(user_id)} has been deleted"
+
+
+
