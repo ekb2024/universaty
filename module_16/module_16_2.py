b7d9776a4785c10f1cc9aee2c9ceaec6c9d46f83
ekb2024 <igor_nu@bk.ru>
2024-12-17 13:27:01 +0500

    Update module_16_2.py

--- module_16/module_16_2.py
+++ module_16/module_16_2.py
@@ -5,17 +5,17 @@ from typing import Annotated
 app = FastAPI()
 
 @app.get("/")
-async  def gstr() ->dict:
-    return {"message":"Главная страница"}
+async  def gst_gs() ->str:
+    return  "Главная страница"
 @app.get("/user/admin")
-async  def  user_admin() ->dict:
-    return {"message": "Вы вошли как администратор"}
+async  def  get_admin() ->str:
+    return   "Вы вошли как администратор"
 
 @app.get("/user/{user_id}")
-async  def  user_admin(user_id:Annotated[int,Path(ge=1, le=100,description="Enter User ID",example="1")]) ->dict:
-    return {"message": f"Вы вошли как пользователь № {user_id}"}
+async  def  get_id(user_id:Annotated[int,Path(ge=1, le=100,description="Enter User ID", example="1")]) ->str
+    return   f"Вы вошли как пользователь № {user_id}"
 
 @app.get("/user/{username}/{age}")
-async  def  user_admin(username : Annotated[str,Path(min_length=5,max_length=20,description='Enter username' ,example='UrbanUser' )]
-                       ,age:Annotated[int,Path(ge=18, le=120, description='Enter age' ,example='24')]) ->dict:
-    return {"message": f"Информация о пользователе. Имя:  {username} Возраст: {age}"}
+async  def  get_username(username : Annotated[str, Path(min_length=5, max_length=20, description='Enter username' ,example='UrbanUser' )]
+                       ,age:Annotated[int, Path(ge=18, le=120, description='Enter age' ,example='24')]) ->str:
+    return f"Информация о пользователе. Имя:  {username} Возраст: {age}"
