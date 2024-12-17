bf9fa72fa383ceea7c87bb3f13c9b376008a5af7
ekb2024 <igor_nu@bk.ru>
2024-12-17 13:27:49 +0500

    Update module_16_2.py

--- module_16/module_16_2.py
+++ module_16/module_16_2.py
@@ -12,10 +12,10 @@ async  def  get_admin() ->str:
     return   "Вы вошли как администратор"
 
 @app.get("/user/{user_id}")
-async  def  get_id(user_id:Annotated[int,Path(ge=1, le=100,description="Enter User ID", example="1")]) ->str
+async  def  get_id(user_id:Annotated[int,Path(ge=1, le=100, description="Enter User ID", example="1")]) ->str
     return   f"Вы вошли как пользователь № {user_id}"
 
 @app.get("/user/{username}/{age}")
-async  def  get_username(username : Annotated[str, Path(min_length=5, max_length=20, description='Enter username' ,example='UrbanUser' )]
+async  def  get_username(username : Annotated[str, Path(min_length=5, max_length=20, description='Enter username', example='UrbanUser' )]
                        ,age:Annotated[int, Path(ge=18, le=120, description='Enter age' ,example='24')]) ->str:
     return f"Информация о пользователе. Имя:  {username} Возраст: {age}"
