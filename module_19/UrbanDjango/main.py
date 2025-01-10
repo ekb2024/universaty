f4a7db829f893bf3e3bc720c401203d115116d7a
ekb2024 <igor_nu@bk.ru>
2025-01-10 18:16:13 +0500

    Add files via upload

---
+++ module_19/UrbanDjango/main.py
@@ -0,0 +1,22 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+
+#django-admin startproject UrbanDjango
+#cd UrbanDjango
+#python manage.py runserver
+#python manage.py startapp exemple1
+#python manage.py migrate
+#python manage.py makemigrations
